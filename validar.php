a844d26a375021849b4f911cafc43822ef188953
Augusto <augus_borromeo@hotmail.com>
2022-04-01 02:23:14 -0300

    base de datos conectada exitosamente con la aplicación. Faltan las validaciones

---
+++ validar.php
@@ -0,0 +1,12 @@
+<?php
+    $usuario = $_POST["nombre"];
+    $password = $_POST["password"];
+    // if(isset($_POST['submit'])) {
+    //     if(empty($usuario)) {
+    //         echo "<h1>BIENVENIDO 😀</h1>";
+    //     } else {
+    //         echo "<h1>CARACTERES INSUFICIENTES 😯</h1>";
+    //     }
+    // }
+    echo $usuario;
+?>
