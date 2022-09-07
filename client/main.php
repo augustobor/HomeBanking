447b47de2788335e8cca242d9dc14c43be90e0c9
augustobor <augus_borromeo@hotmail.com>
2022-09-07 14:15:19 -0300

    design optimized

--- client/main.php
+++ client/main.php
@@ -6,11 +6,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="description" content="home-banking-client">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="icon" href="../favicon.ico">
+    <link rel="icon preconnect" href="../favicon.ico">
 
     <link rel="stylesheet" type="text/css" href="./styles/home_mobile.css">
     <link rel="stylesheet" type="text/css" href="./styles/home_tablet.css" media="screen and (min-width: 680px)">
@@ -51,6 +52,6 @@
             </div>
         </section>
     </main>
-    <script src='./lista.js'></script>
+    <script async src='./lista.js'></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,13 +4,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="home-banking-login">
+    <meta name="charset" charset="UTF-8">
+    <meta name="UX" http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="icon" href="./favicon.ico">
 
-    <link rel="stylesheet" type="text/css" href="./login/styles/style.css">
+    <link rel="stylesheet" type="text/css" href="./login/styles/style.css" media>
     <link rel="stylesheet" type="text/css" href="./login/styles/tablet.css" media="screen and (min-width: 680px)">
     <link rel="stylesheet" type="text/css" href="./login/styles/desktop.css" media="screen and (min-width: 1000px)"> 
 
