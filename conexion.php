7b9c51ab38d8f55fc96a59c43cdf71c1ef40428c
Augusto <64516928+augustobor@users.noreply.github.com>
2022-10-07 18:09:02 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-    session_start()
+    session_start();
     $conexion = mysqli_connect($_ENV['MYSQLHOST'], $_ENV['MYSQLUSER'], $_ENV['MYSQLPASSWORD'], $_ENV['MYSQLDATABASE'], $_ENV['MYSQLPORT']);
 ?>
 
