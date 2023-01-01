369d8a1fe96e27a52280e8fe5300005b1255d36e
augustobor <augus_borromeo@hotmail.com>
2023-01-01 03:30:39 -0300

    readme updated

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM php:7.2-apache
 
-RUN apt-get update && apt-get install -y libpq-dev && docker-php-ext-install pdo pdo_pgsql pgsql
+RUN apt-get update && apt-get install -y libpq-dev && docker-php-ext-install pdo mysqli
 COPY /src /var/www/html
 EXPOSE 80
--- src/conexion.php
+++ src/conexion.php
@@ -1,9 +1,9 @@
 <?php
     $conexion = mysqli_connect(
-        'containers-us-west-19.railway.app', 
-        'root', 
-        '7pR4i9tfQigV31SCWL7X', 
-        'railway', 
-        '7705');
+        $_ENV['MYSQLHOST'],
+        $_ENV['MYSQLUSER'],
+        $_ENV['MYSQLPASSWORD'],
+        $_ENV['MYSQLDATABASE'],
+        $_ENV['MYSQLPORT']);
 ?>
 
